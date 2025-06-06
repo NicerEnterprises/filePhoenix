fd9d6839d4249ebf7f763108b7d0be188650fe98
Rene A.J.M. Veerman <rene.veerman.netherlands@gmail.com>
2025-06-06 03:11:44 +0200

    Upgrades in output geerating routines

---
+++ app.dialog.siteContent.title.php
@@ -0,0 +1,1 @@
+FilePhoenix by NicerEnterprises.
--- cronjobs_calculate_traceroute_averages.php
+++ cronjobs_calculate_traceroute_averages.php
@@ -31,7 +31,14 @@ function naWebOS_gather_desktop_OS_info() {
         'output' => $output,
         'result_code' => $result_code
     ];
-    naWebOS_output_debug_info ($di);
+    $cmd = [
+        'di' => $di,
+        '{"HTML_className":"naWebOS-debug-outer-DIV"}' => 'naWebOS-debug-outer-DIV',
+        '{"HTML_className":"naWebOS-field-name"}' => 'naWebOS-field-name',
+        '{"HTML_className":"naWebOS-field-value"}' => 'naWebOS-field-value',
+        '{"HTML_className":"naWebOS-debug-lineRemaining"}' => 'naWebOS-debug-lineRemaining'
+    ];
+    naWebOS_output_debug_info ($cmd);
 
     /*
     $regEx = '/\s+\([a-z][A-Z[0-0]\):\s([a-z][A-Z[0-0]\)./)';
@@ -55,7 +62,14 @@ function naWebOS_gather_traceroute_version () {
         'output' => $output,
         'result_code' => $result_code
     ];
-    naWebOS_output_debug_info ($di);
+    $cmd = [
+        'di' => $di,
+        '{"HTML_className":"naWebOS-debug-outer-DIV"}' => 'naWebOS-debug-outer-DIV',
+        '{"HTML_className":"naWebOS-field-name"}' => 'naWebOS-field-name',
+        '{"HTML_className":"naWebOS-field-value"}' => 'naWebOS-field-value',
+        '{"HTML_className":"naWebOS-debug-lineRemaining"}' => 'naWebOS-debug-lineRemaining'
+    ];
+    naWebOS_output_debug_info ($cmd);
 
     $expectedOutputs = [
         0    => [
@@ -84,31 +98,48 @@ function naWebOS_gather_traceroute_data_for_VPN_outgoing_connections ($target='G
         'output' => $output,
         'result_code' => $result_code
     ];
-    naWebOS_output_debug_info ($di);
+    $cmd = [
+        'di' => $di,
+        '{"HTML_className":"naWebOS-debug-outer-DIV"}' => 'naWebOS-debug-outer-DIV',
+        '{"HTML_className":"naWebOS-field-name"}' => 'naWebOS-field-name',
+        '{"HTML_className":"naWebOS-field-value"}' => 'naWebOS-field-value',
+        '{"HTML_className":"naWebOS-debug-lineRemaining"}' => 'naWebOS-debug-lineRemaining'
+    ];
+    naWebOS_output_debug_info ($cmd);
 
     foreach ($output as $idx => $line) {
         if ($idx===0) {
             $regEx_targetIP = '/(.*\))/';
             $preg_match_result_code = preg_match ($regEx_targetIP, $line, $matches, PREG_OFFSET_CAPTURE);
             $di = [
+                '$idx' => $idx,
+                '$line' => $line,
                 '$regEx_targetIP' => $regEx_targetIP,
                 'mode' => 'PREG_OFFSET_CAPTURE',
                 '$matches' => $matches,
                 '$preg_match_result_code' => $preg_match_result_code
             ];
-            naWebOS_output_debug_info ($di);
+            $cmd['di']= $di;
+            naWebOS_output_debug_info ($cmd);
         }
 
         if ($idx > 0) {
-            $regEx_lineDecompiledRXdata = '/\s+"\s+\d+\s+([\d.]+)".*/';
+            $line1a = $line;
+            $regEx_lineDecompiledRXdata = '/\s+([\-\w\d]+)\s+(([\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+)\s+(\([\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+\))\s+(\d+\.\d+\sms))\s+/';
             $preg_match_result_code = preg_match ($regEx_lineDecompiledRXdata, $line, $matches, PREG_OFFSET_CAPTURE);
             $di = [
+                '$idx' => $idx,
+                '$line' => $line,
                 '$regEx_lineDecompiledRXdata' => $regEx_lineDecompiledRXdata,
                 'mode' => 'PREG_OFFSET_CAPTURE',
                 '$matches' => $matches,
                 '$preg_match_result_code' => $preg_match_result_code
             ];
-            naWebOS_output_debug_info ($di);
+            $line1a = str_replace ($matches[0][0],'',$line1a);
+            $cmd['di']= $di;
+            $cmd['{"value:linesRemaining"}'] = $line1a;
+            naWebOS_output_debug_info ($cmd);
+
         }
     }
 }
