199668d03cce3ec8f301fc6c4ec41964b5ed4621
Rene A.J.M. Veerman <rene.veerman.netherlands@gmail.com>
2025-06-08 11:38:15 +0200

    many useful upgrades - untested due to local internet problems

--- app.filePhoenix.dialog.siteContent.php
+++ app.filePhoenix.dialog.siteContent.php
@@ -1,5 +1,3 @@
 <?php
-    set_time_limit(100);
-    require_once(dirname(__FILE__).'/../boot.php');
     require_once (dirname(__FILE__).'/cronjobs_calculate_traceroute_averages.php');
 ?>
--- cronjobs_calculate_traceroute_averages.php
+++ cronjobs_calculate_traceroute_averages.php
@@ -7,22 +7,55 @@ global $naWebOS;
 
 $fn = realpath(dirname(__FILE__)).'/naWebOS_traceroute_data_gathering.css';
 $fnWeb = $naWebOS->adjustPath($fn);
-echo '<!DOCTYPE html>';
-echo '<head>';
-echo '<link type="text/css" rel="StyleSheet" href="'.$fnWeb.'?m='.$naWebOS->fileDateTimeStamp($fn).'"   >'.PHP_EOL;
-echo '</head>';
-echo '<body>';
 
-naWebOS_gather_traceroute_data();
+//MAIN:
+echo '<link type="text/css" rel="StyleSheet" href="'.$fnWeb.'?m='.$naWebOS->fileDateTimeStamp($fn).'"   >'.PHP_EOL;
+echo naWebOS_gather_traceroute_data();
+
+
 
 function naWebOS_gather_traceroute_data () {
-    naWebOS_gather_desktop_OS_info();
-    naWebOS_gather_traceroute_version();
-    naWebOS_gather_traceroute_data_for_VPN_outgoing_connections ('Google.com');
-    //naWebOS_gather_traceroute_data_for_VPN_outgoing_connections ('HotMail.com');
+    $cmd = [
+        '{"HTML_className":"naWebOS-debug-outer-DIV"}' => 'naWebOS-debug-outer-DIV',
+        '{"HTML_className":"naWebOS-field-name"}' => 'naWebOS-field-name',
+        '{"HTML_className":"naWebOS-field-value"}' => 'naWebOS-field-value',
+        '{"HTML_className":"naWebOS-debug-lineRemaining"}' => 'naWebOS-debug-lineRemaining',
+        '{"value":"linesRemaining"}' => 'color:ivory;background:rgba(255,0,0,0.4);border-radius:5px;box-shadow:2px 2px 4px 3px rgba(0,0,0,0.555)',
+        '{"color":"hostnamectl"}' => 'color:white;background:rgba(255,0,0,0.4);border-radius:10px;box-shadow:2px 2px 4px 3px rgba(0,0,0,0.555)',
+        '{"color":"tracerouteVersion"}' => 'color:yellow;background:rgba(255,255,0,0.7);border-radius:5px;box-shadow:2px 2px 4px 3px rgba(0,0,0,0.555)',
+        '{"color":"tracerouteResults"}' =>'color:white;font-weight:bold;background:rgba(0,0,255,0.4);border-radius:5px;box-shadow:2px 2px 4px 3px rgba(0,0,0,0.555)',
+        '{"color":"tracerouteResultLine"}' => 'color:ivory;background:rgba(255,255,50,0.4);border-radius:5px;box-shadow:2px 2px 4px 3px rgba(0,0,0,0.555)'
+    ];
+    $cmd['{"colorSelection_outerDiv":"determinedInUpwardFunction"}'] = 'hostnamectl';
+
+    $r = naWebOS_gather_desktop_OS_info($cmd);
+
+    $cmd['{"colorSelection_outerDiv":"determinedInUpwardFunction"}'] = 'traceroute --version';
+    $r .= naWebOS_gather_traceroute_version($cmd);
+
+    $destinations = [
+        'www.freedomnet.nl'/*,
+        'google.nl',
+        'google.nl',
+        'gmail.com',
+        'drive.google.com',
+        'drive.google.nl',
+        'images.google.com',
+        'images.google.nl',
+        'hotmail.com',
+        'hotmail.nl',
+        'bing.com',
+        'bing.nl'*/
+    ];
+    foreach ($destinations as $destIdx => $destAddress) {
+        $cmd['target'] = $destAddress;
+        $cmd['{"colorSelection_outerDiv":"determinedInUpwardFunction"}'] = '0:tracerouteResults,N:tracerouteResultLine'; //CSV format for value
+        $r .= naWebOS_gather_traceroute_data_for_VPN_outgoing_connections ($cmd);
+    }
+    return $r;
 }
 
-function naWebOS_gather_desktop_OS_info() {
+function naWebOS_gather_desktop_OS_info($cmd) {
     $xec = 'hostnamectl';
     exec ($xec, $output, $result_code);
     $di = [
@@ -31,14 +64,9 @@ function naWebOS_gather_desktop_OS_info() {
         'output' => $output,
         'result_code' => $result_code
     ];
-    $cmd = [
-        'di' => $di,
-        '{"HTML_className":"naWebOS-debug-outer-DIV"}' => 'naWebOS-debug-outer-DIV',
-        '{"HTML_className":"naWebOS-field-name"}' => 'naWebOS-field-name',
-        '{"HTML_className":"naWebOS-field-value"}' => 'naWebOS-field-value',
-        '{"HTML_className":"naWebOS-debug-lineRemaining"}' => 'naWebOS-debug-lineRemaining'
-    ];
-    naWebOS_output_debug_info ($cmd);
+    $cmd['di'] = $di;
+    $cmd['target'] = $xec;
+    return naWebOS_output_debug_command ($cmd);
 
     /*
     $regEx = '/\s+\([a-z][A-Z[0-0]\):\s([a-z][A-Z[0-0]\)./)';
@@ -53,8 +81,8 @@ function naWebOS_gather_desktop_OS_info() {
     */
 }
 
-function naWebOS_gather_traceroute_version () {
-    $xec = 'traceroute --v';
+function naWebOS_gather_traceroute_version ($cmd=null) {
+    $xec = 'traceroute --version';
     exec ($xec, $output, $result_code);
     $di = [
         'mainPreClassName' => 'naWebOS_traceroute_version',
@@ -62,15 +90,11 @@ function naWebOS_gather_traceroute_version () {
         'output' => $output,
         'result_code' => $result_code
     ];
-    $cmd = [
-        'di' => $di,
-        '{"HTML_className":"naWebOS-debug-outer-DIV"}' => 'naWebOS-debug-outer-DIV',
-        '{"HTML_className":"naWebOS-field-name"}' => 'naWebOS-field-name',
-        '{"HTML_className":"naWebOS-field-value"}' => 'naWebOS-field-value',
-        '{"HTML_className":"naWebOS-debug-lineRemaining"}' => 'naWebOS-debug-lineRemaining'
-    ];
-    naWebOS_output_debug_info ($cmd);
+    $cmd['di'] = $di;
+    $cmd['target'] = $xec;
+    return naWebOS_output_debug_command ($cmd);
 
+    /*
     $expectedOutputs = [
         0    => [
             'traceroute (GNU inetutils) 2.5',
@@ -88,60 +112,107 @@ function naWebOS_gather_traceroute_version () {
         if ($result_code === $k && $v == $output[0][$k]) $cr = true;
     }
     return $cr;
+    */
 }
 
-function naWebOS_gather_traceroute_data_for_VPN_outgoing_connections ($target='Google.com') {
-    $xec = 'traceroute '.$target;
+function naWebOS_gather_traceroute_data_for_VPN_outgoing_connections ($cmd=null) {
+    $fncn = 'naWebOS_gather_traceroute_data_for_VPN_outgoing_connections';
+    if (!is_array($cmd)) trigger_error ($fncn.' : $cmd is not an [k=>v,k=>v,...] array.');
+    $xec = 'traceroute '.$cmd['target'];
     exec ($xec, $output, $result_code);
     $di = [
+        'idx' => 0,
         'execString' => $xec,
         'output' => $output,
         'result_code' => $result_code
     ];
-    $cmd = [
-        'di' => $di,
-        '{"HTML_className":"naWebOS-debug-outer-DIV"}' => 'naWebOS-debug-outer-DIV',
-        '{"HTML_className":"naWebOS-field-name"}' => 'naWebOS-field-name',
-        '{"HTML_className":"naWebOS-field-value"}' => 'naWebOS-field-value',
-        '{"HTML_className":"naWebOS-debug-lineRemaining"}' => 'naWebOS-debug-lineRemaining'
-    ];
-    naWebOS_output_debug_info ($cmd);
+    $cmd['di'] = $di;
+    $r = '<h1>'.$output[0].'</h1>'.PHP_EOL.PHP_EOL;
+    $r .= naWebOS_output_debug_command ($cmd);
 
-    foreach ($output as $idx => $line) {
-        if ($idx===0) {
-            $regEx_targetIP = '/(.*\))/';
-            $preg_match_result_code = preg_match ($regEx_targetIP, $line, $matches, PREG_OFFSET_CAPTURE);
-            $di = [
-                '$idx' => $idx,
-                '$line' => $line,
-                '$regEx_targetIP' => $regEx_targetIP,
-                'mode' => 'PREG_OFFSET_CAPTURE',
-                '$matches' => $matches,
-                '$preg_match_result_code' => $preg_match_result_code
-            ];
-            $cmd['di']= $di;
-            naWebOS_output_debug_info ($cmd);
-        }
+    $idx = 1;
+    while ($idx >= 1 && array_key_exists($idx,$output)) {
+        $line = $output[$idx];
+        $line1a = $line;
+        $tl = trim($line1a);
+        $sectionComplete = false;
 
-        if ($idx > 0) {
-            $line1a = $line;
-            $regEx_lineDecompiledRXdata = '/\s+([\-\w\d]+)\s+(([\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+)\s+(\([\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+\.[\-\w\d]+\))\s+(\d+\.\d+\sms))\s+/';
-            $preg_match_result_code = preg_match ($regEx_lineDecompiledRXdata, $line, $matches, PREG_OFFSET_CAPTURE);
-            $di = [
-                '$idx' => $idx,
-                '$line' => $line,
-                '$regEx_lineDecompiledRXdata' => $regEx_lineDecompiledRXdata,
-                'mode' => 'PREG_OFFSET_CAPTURE',
-                '$matches' => $matches,
-                '$preg_match_result_code' => $preg_match_result_code
-            ];
-            $line1a = str_replace ($matches[0][0],'',$line1a);
-            $cmd['di']= $di;
-            $cmd['{"value:linesRemaining"}'] = $line1a;
-            naWebOS_output_debug_info ($cmd);
+        while ($tl!='') {
 
+            $line1a = preg_replace('/\s+\d+\s+/','',$line1a);
+
+            $matches2 = [];
+            while (is_array($matches2) && count($matches2)===0) {
+                $regEx_linePartData = '/([\-\w\d\.]+)\s+(\([\w\d]+\.[\w\d]+\.[\w\d]+\.[\w\d]+\))/';
+                $preg_match_result_code = preg_match ($regEx_linePartData, $line1a, $matches2, PREG_OFFSET_CAPTURE);
+                if (is_array($matches2) && count($matches2)!==0) {
+                    $line1a = str_replace($matches2[0][0],'',$line1a);
+                    $tl = trim($line1a);
+                    $line1b = $line1a;
+                    $di = [
+                        '$idx' => $idx,
+                        '$idx2' => '[NULL]',
+                        '$regEx_linePartMatch' => $matches2[0][0],
+                        '$regEx_linePartData' => $regEx_linePartData,
+                        '$mode' => 'PREG_OFFSET_CAPTURE',
+                        '$preg_match_result_code' => $preg_match_result_code,
+                        '$matches' => $matches2,
+                        '$lineRemaining' => $tl
+                    ];
+                    $cmd['di'] = $di;
+                    $r .= naWebOS_output_debug_info ($cmd);
+                }
+
+                flush();
+                ob_flush();
+                ob_end_flush();
+                ob_end_clean();
+                ob_start();
+
+            }
+
+            $line1a = $tl;
+            $idx2 = 0;
+            while ($tl!=='' && !$sectionComplete) {
+
+                if ($tl==($idx).'  * * *') {
+                    $r .= '<p>'.($idx).' * * * [UNREACHABLE]</p>';
+                    $idx2++;
+                    break;
+                } else {
+                    $matches3 = [];
+                    $regEx_linePartMS = '/\s*(\d+[.,]\d+)\s+ms\s*/';
+                    $preg_match_result_code = preg_match ($regEx_linePartMS, $line1a, $matches3, PREG_OFFSET_CAPTURE);
+                    if (is_array($matches3) && count($matches3)!==0) {
+                        $line1a = str_replace($matches3[0][0],'',$line1a);
+                        $tl = trim($line1a);
+                        $line1b = $line1a;
+                        $di = [
+                            '$idx' => $idx,
+                            '$idx2' => $idx2,
+                            '$regEx_linePartMatch' => $matches3[0][0],
+                            '$regEx_linePartMS' => $regEx_linePartMS,
+                            '$mode' => 'PREG_OFFSET_CAPTURE',
+                            '$preg_match_result_code' => $preg_match_result_code,
+                            '$matches' => $matches3,
+                            '$lineRemaining' => $tl
+                        ];
+                        $cmd['di'] = $di;
+                        $idx2++;
+                        $r .= naWebOS_output_debug_info ($cmd);
+                    } else { $sectionComplete = true; };
+                }
+
+                flush();
+                ob_flush();
+                ob_end_flush();
+                ob_end_clean();
+                ob_start();
+
+            }
         }
     }
+    return $r;
 }
 
 function naWebOS_gather_traceroute_data_for_regular_outgoing_connections () {
